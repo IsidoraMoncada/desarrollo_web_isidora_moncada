ae782096d4d872332310afbd450cd73e5674b111
IsidoraMoncada <isidora.moncada@ug.uchile.cl>
2025-05-10 22:10:25 -0400

    Agregué archivo requirements.txt

--- js/listado.js
+++
@@ -1,6 +0,0 @@
-const validarvolver = () => {
-    window.location.href = "./index.html";  
-};
-
-let submitBtn1 = document.getElementById("volver");
-submitBtn1.addEventListener("click", validarvolver);
